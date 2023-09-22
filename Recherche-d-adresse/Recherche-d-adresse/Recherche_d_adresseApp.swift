2217be763dfaa95fc6447c7fd8c541c6a388a8f3
Borislav Borisov <bborisov@gameaccount.com>
2023-09-22 15:38:58 +0300

    Add: Project init

---
+++ Recherche-d-adresse/Recherche-d-adresse/Recherche_d_adresseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recherche_d_adresseApp.swift
+//  Recherche-d-adresse
+//
+//  Created by Borislav Borisov on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Recherche_d_adresseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
