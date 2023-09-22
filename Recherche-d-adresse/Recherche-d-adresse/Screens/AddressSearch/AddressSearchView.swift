b032a59fe8668653605dd0f7e5ce9ee669880966
Borislav Borisov <bborisov@gameaccount.com>
2023-09-22 16:57:48 +0300

    Add: AddressSearchView

--- Recherche-d-adresse/Recherche-d-adresse/Screens/AddressSearch/AddressSearchView.swift
+++ Recherche-d-adresse/Recherche-d-adresse/Screens/AddressSearch/AddressSearchView.swift
@@ -6,13 +6,38 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct AddressSearchView: View {
+
+    @StateObject var viewModel: AddressSearchViewModel
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            searchField
+            searchResults
+        }
+    }
+    
+    // MARK: - Private
+    
+    private var searchField: some View {
+        TextField("Search for an address", text: $viewModel.query)
+            .padding()
+            .background(Color(.systemGray6))
+            .cornerRadius(8)
+            .padding(.horizontal)
+    }
+    
+    private var searchResults: some View {
+        List(viewModel.searchResults, id: \.id) { result in
+            Text(result.label)
+        }
     }
 }
 
-#Preview {
-    AddressSearchView()
+struct AddressSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressSearchView(viewModel: AddressSearchViewModel(addressAPI: AddressAPI(managerAPI: ManagerAPI())))
+    }
 }
